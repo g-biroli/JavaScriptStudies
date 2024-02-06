27da691706313543eb316fc61d656fae3eb4a3f7
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-02-06 02:38:59 +0100

    refresh

--- Lenguaje/hola_mundo.js
+++ Lenguaje/hola_mundo.js
@@ -22,6 +22,8 @@ console.log(typeof numero);
 
 // segun el dato que guarda -> primarias / complejas
 
+
+//variables:
 // numeros -> int -> number (int, float, double, byte)
 // palabras -> String -> string
 // boolean -> boolean
@@ -32,3 +34,15 @@ console.log(typeof numero);
 // let nombre; undefined
 // String nombre; // null
 // nombre.length
+
+// -> MODIFICANDO O PARAGRAFO DO DOCUMENTO INDEX
+// -> para trazer um item do outro arquivo é necessario usar a funcao selector
+// 1- Tener el nodo 2- obtengo un nodo que cumpla con el selector
+let parrafo = document.querySelector("p"); //busca por TAG
+parrafo.textContent = "Cambiando contenido del parrafo mediante JS";
+// Si quiero encontrar uno elemento tiene mas sentido por ID, por que ID es unico
+let parrafoDos = document.querySelector("#parrafo-dos");
+parrafoDos.textContent = "Este cambio va a verse en el parrafo dos";
+let parrafoTres = document.querySelector(".parrafos");
+parrafoTres.textContent = "Este cambio se muestra en el tres"
+
