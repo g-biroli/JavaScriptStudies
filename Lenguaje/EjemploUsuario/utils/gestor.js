84b0eeba2c84eab33afdc7a11843234afd4de76f
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-02-20 00:15:40 +0100

    clase javascript

---
+++ Lenguaje/EjemploUsuario/utils/gestor.js
@@ -0,0 +1,17 @@
+let inputNombre = document.querySelector("input#input-nombre");
+let inputApellido = document.querySelector("input#input-apellido");
+let boton = document.querySelector("#boton-agregar")
+let listaUsuarios = document.querySelector("#lista-usuarios")
+//Adicionando os botoes e o espaco de armazenamento da resposta
+
+boton.addEventListener("click", () => {
+    console.log(inputNombre.value);
+    console.log(inputApellido.value);
+   if (inputNombre.value.length > 0 && inputApellido.value.length > 0 ){
+    listaUsuarios.innerHTML += `<li class='list-group-item'> ${inputNombre.value} ${inputApellido.value}</li>`;
+    inputApellido.value = "";   //Este input foi criado para que apos aparecer o nome na lista
+    inputNombre.value = "";     //Nao seja permitido que spame o mesmo nome na lista varias vezes
+} else {
+    alert ("no se puede anadir")
+   }
+})
