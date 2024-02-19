e10d9cf097cf9967d3aa050e1648ce4ef86d3100
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-02-20 00:39:31 +0100

    save

--- Lenguaje/EjemploUsuario/utils/gestor.js
+++ Lenguaje/EjemploUsuario/utils/gestor.js
@@ -8,7 +8,7 @@ boton.addEventListener("click", () => {
     console.log(inputNombre.value);
     console.log(inputApellido.value);
    if (inputNombre.value.length > 0 && inputApellido.value.length > 0 ){
-    listaUsuarios.innerHTML += `<li class='list-group-item'> ${inputNombre.value} ${inputApellido.value}</li>`;
+    listaUsuarios.innerHTML += `<li class='animate__animated animate__zoomInDown list-group-item'> ${inputNombre.value} ${inputApellido.value}</li>`;
     inputApellido.value = "";   //Este input foi criado para que apos aparecer o nome na lista
     inputNombre.value = "";     //Nao seja permitido que spame o mesmo nome na lista varias vezes
 } else {
