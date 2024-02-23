6a86dbcba202ed9e7b8addf4111cbe45e927a548
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-02-23 19:13:11 +0100

    Nueva Tarea
    Ejercicio avaluable Universidad Europea

---
+++ Lenguaje/EjercicioAvaluable2/utils/gestor.js
@@ -0,0 +1,4 @@
+let inputNombre = document.querySelector("input#input-nombre");
+let inputApellido = document.querySelector("input#input-apellido");
+let inputCorreo = document.querySelector("input#input-correo");
+let boton = document.querySelector("#boton-agregar");
