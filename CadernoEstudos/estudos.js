635294fa00c01fe594cda60370c543dd20d1a5a4
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-03-20 00:55:58 +0100

    ForEach + Form action + Lista de dados da array
    Exemplos de funcao ForEach com açoes dentro de arrays + metodo action no forms para os arquivos irem para a parte do servidor. Metodo em js para listar os dados do array (pedido na atividade avaliativa 2)

--- CadernoEstudos/estudos.js
+++ CadernoEstudos/estudos.js
@@ -1,11 +1,21 @@
-// JSON -> javascript object notation
-// {} -> es un objeto
-// [] -> es un array
-// [{}, {}, {}] -> es un array de objetos
-
-let lista = document.querySelector("#lista")
-
+// Exemplo de como usar ForEach em conjunto com array
 let objeto = {
+    nombre: "Gabriel",
+    apellido: "Biroli",
+    edad: 24,
+    carnet: true,
+    conocimientos: ["programacion", "3d", "sistemas", "gestion"],
+};
+
+//ForEach para pegar todos os dados da arraylist e escrever no console separado
+console.log(objeto.nombre);  
+objeto.conocimientos.forEach((item) =>{
+    console.log(item);
+})
+
+// Exemplo de ForEach com outra arraylist
+
+/* let objeto = {
     nombre: "Gabriel",
     apellido: "Biroli",
     edad: 24,
@@ -36,19 +46,15 @@ let usuarios = [
     conocimientos: ["cocinar", "musica", "matematica", "gestion"],
    },
 ]
-// metodo para litar na pagina web os dados da array (atividade evaluable)
-usuarios.forEach ((item) => {
-    lista.innerHTML += `<li>${item.nombre} ${item.apellido}</li>`;
-})
- 
-/* usuarios.forEach((item) =>{
+
+usuarios.forEach((item) =>{
     console.log(item.nombre)
     item.conocimientos.forEach((item) => {
         console.log(item);
     })
-})
+})  */
 
-console.log(objeto.nombre);  //ForEach para pegar todos os dados da arraylist e escrever no console separado
-objeto.conocimientos.forEach((item) =>{
-    console.log(item);
-}) */
+// metodo para litar na pagina web os dados da array (atividade evaluable)
+usuarios.forEach ((item) => {
+    lista.innerHTML += `<li>${item.nombre} ${item.apellido}</li>`;
+})
